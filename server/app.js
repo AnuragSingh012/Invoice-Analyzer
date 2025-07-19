0d57e92f9001d047bd0765e8bf401faf1bc15e56
Anurag Singh <anuragsingh4932@gmail.com>
2025-07-19 22:32:00 +0530

    fix bug

--- server/app.js
+++ server/app.js
@@ -4,14 +4,15 @@ import multer from 'multer';
 import invoiceRoutes from './routes/invoiceRoutes.js';
 
 const app = express();
-const PORT = 3000;
+const upload = multer({ dest: 'uploads/' });
 
 app.use(cors());
 app.use(express.json());
 
-const upload = multer({ dest: 'uploads/' });
+app.get('/', (req, res) => {
+  res.send('App is running...');
+});
+
 app.use('/api/invoice', upload.single('invoice'), invoiceRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+export default app;
